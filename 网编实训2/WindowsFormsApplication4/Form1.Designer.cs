86dcfe740cb45335632342f3e25bcc06ede138cc
Joe <738380029@qq.com>
2018-01-10 20:52:44 +0800

    finish

---
+++ 网编实训2/WindowsFormsApplication4/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace WindowsFormsApplication4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxIP = new System.Windows.Forms.TextBox();
+            this.textBoxMessage = new System.Windows.Forms.TextBox();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.呼叫对方ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.结束程序ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(141, 133);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "目标IP地址：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(141, 178);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "呼叫信息：";
+            // 
+            // textBoxIP
+            // 
+            this.textBoxIP.Location = new System.Drawing.Point(224, 130);
+            this.textBoxIP.Name = "textBoxIP";
+            this.textBoxIP.Size = new System.Drawing.Size(100, 21);
+            this.textBoxIP.TabIndex = 2;
+            // 
+            // textBoxMessage
+            // 
+            this.textBoxMessage.Location = new System.Drawing.Point(224, 169);
+            this.textBoxMessage.Name = "textBoxMessage";
+            this.textBoxMessage.Size = new System.Drawing.Size(100, 21);
+            this.textBoxMessage.TabIndex = 3;
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(187, 240);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(75, 23);
+            this.buttonSend.TabIndex = 4;
+            this.buttonSend.Text = "发送";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.呼叫对方ToolStripMenuItem,
+            this.结束程序ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(119, 48);
+            // 
+            // 呼叫对方ToolStripMenuItem
+            // 
+            this.呼叫对方ToolStripMenuItem.Name = "呼叫对方ToolStripMenuItem";
+            this.呼叫对方ToolStripMenuItem.Size = new System.Drawing.Size(118, 22);
+            this.呼叫对方ToolStripMenuItem.Text = "呼叫对方";
+            this.呼叫对方ToolStripMenuItem.Click += new System.EventHandler(this.呼叫对方ToolStripMenuItem_Click);
+            // 
+            // 结束程序ToolStripMenuItem
+            // 
+            this.结束程序ToolStripMenuItem.Name = "结束程序ToolStripMenuItem";
+            this.结束程序ToolStripMenuItem.Size = new System.Drawing.Size(118, 22);
+            this.结束程序ToolStripMenuItem.Text = "结束程序";
+            this.结束程序ToolStripMenuItem.Click += new System.EventHandler(this.结束程序ToolStripMenuItem_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "网络呼叫应答";
+            this.notifyIcon1.Visible = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(502, 317);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.textBoxMessage);
+            this.Controls.Add(this.textBoxIP);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "网络呼叫应答";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxIP;
+        private System.Windows.Forms.TextBox textBoxMessage;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 呼叫对方ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 结束程序ToolStripMenuItem;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ 网编实训2/WindowsFormsApplication4/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Net;
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form1 : Form
+    {
+         
+          private volatile bool isExit;
+          private int port = 8888;
+          private UdpClient myUdpClient;
+
+        public Form1()
+        {
+            isExit = false;
+            port = 8888;
+            myUdpClient = new UdpClient(port);
+            InitializeComponent();
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Thread t = new Thread(ReceiveData);
+            t.IsBackground = true;
+            t.Start();
+
+        }
+       private void ReceiveData()
+       {
+               IPEndPoint iep = null;
+              while (!isExit)
+            {
+                try
+                {
+                    byte[] bytes = myUdpClient.Receive(ref iep);
+                    string message = Encoding.Unicode.GetString(bytes);
+                    MessageBox.Show("收到来自" + iep.Address + "的信息：" + message);
+                }
+                catch
+                {
+                    MessageBox.Show("停止接收信息！");
+                    break;
+                }
+            }
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            IPAddress remoteIP;
+         string message = textBoxMessage.Text; ;
+        try
+        {
+        remoteIP = IPAddress.Parse(textBoxIP.Text);
+         }
+    catch
+   {
+        MessageBox.Show("请输入正确的IP地址！");
+        return;
+    }
+    if (message == string.Empty)
+    {
+        MessageBox.Show("请输入需要发送的信息！");
+        return;
+    }
+   SendData(remoteIP, message);
+
+    }
+  private void SendData(IPAddress ip, string message)
+{
+    try
+    {
+        IPEndPoint iep = new IPEndPoint(ip, port);
+        byte[] bytes = Encoding.Unicode.GetBytes(message);
+        myUdpClient.Send(bytes, bytes.Length, iep);
+    }
+    catch(Exception ex)
+    {
+        MessageBox.Show(ex.Message + "发送信息失败！");
+    }
+}
+
+       
+
+        private void 呼叫对方ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           this.Show();
+        }
+
+        private void 结束程序ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           isExit = true;
+         myUdpClient.Close();
+         Application.Exit();
+
+        }
+            private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+            {
+                if (!isExit)
+                {
+                    e.Cancel = true;
+                    this.Hide();
+                }
+
+            }
+
+       
+
+
+    }
+}
